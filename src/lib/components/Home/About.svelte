f8fad9a519ea574518eb23bfc7b31dc7f95a7796
minimo-io <nicolas@minimo.io>
2025-05-15 08:08:50 -0300

    home desktop almost there

---
+++ src/lib/components/Home/About.svelte
@@ -0,0 +1,105 @@
+<section class="fw-home-about fw-border-b-divider-dot mx-auto mb-10 max-w-[60%] pt-10">
+	<p>
+		<span class="fw-text-pixel">W</span>e are an innovation & development lab experimenting in the
+		intersection of e-Commerce, Payments, AI and Machines, with a focus in LATAM.
+	</p>
+
+	<p>
+		<span class="fw-text-pixel">W</span>e have fun experimenting with technology... We build
+		real-world applications & machines.
+	</p>
+
+	<p>
+		We are minimalists & efficient. We aim at building performant systems with little. We use what
+		it's best, we do not reinvent wheels. No charge for it. We are <span
+			class="text-primary text-lg font-bold"
+			><a
+				target="_blank"
+				rel="nofollow noopener"
+				href="https://a16z.com/the-techno-optimist-manifesto/"
+				class="underline hover:opacity-50">techno-optimists</a
+			></span
+		>.
+	</p>
+
+	<p class="font-bold"><span>//</span> We design, we experiment, we ship!</p>
+
+	<p>
+		<span class="fw-text-pixel">W</span>e are also a platform for developing Digital Products. We
+		offer our infrastructure, APIs and libraries to experimental projects.
+	</p>
+
+	<p>
+		Some experiments fail, do not reach the level of products, so they remain as acquired knowledge
+		in the form of libraries, designs, and articles.
+	</p>
+
+	<p>
+		Sometimes ideas do not leave their form, so we let them mature in the <a
+			href="/"
+			class="underline">Idea Lab</a
+		>.
+	</p>
+
+	<p>
+		<span class="fw-text-pixel">F</span>ortunately our base tech stack is very simple yet powerful:
+		<span class="font-bold">Svelte</span>
+		&
+		<span class="font-bold">Figma</span>
+		for the front-end, <span class="font-bold">NestJS</span> for our APIs,
+		<span class="font-bold">Python</span> for AI, <span class="font-bold">MicroPython</span> for
+		embedded systems, and <span class="font-bold">Docker</span> for containerization,
+		<span class="font-bold">Bitcoin</span> & <span class="font-bold">USDT</span> for value
+		preservation & money transmission,
+		<span class="font-bold">Vercel</span>, <span class="font-bold">Render</span> &
+		<span class="font-bold">AWS</span> for cloud infrastructure.
+	</p>
+
+	<p>
+		<span class="fw-text-pixel">W</span>e believe in open-protocols, so we are also experimenting on
+		building for <span class="font-bold">Nostr</span>.
+	</p>
+
+	<p>
+		Sometimes we use our expertise to contribute to other projects. In these Joint Ventures we
+		participate as technological partners, providing our infrastructure, APIs & our team.
+	</p>
+
+	<p>
+		<span class="fw-text-pixel">W</span>e first solve our own problems through technology, and then
+		make valuable products out of it.
+	</p>
+
+	<p>
+		We also curate a list of third party Tools that we ourselves use on a daily basis, aiming to
+		share with others not only our own stuff, but what it's best out there.
+	</p>
+
+	<p>
+		We nurse from open-source, so we contribute to open-source. Many of the libraries we create for
+		our products can be found in our <span class="text-primary text-lg font-bold"
+			><a href="/" class="underline hover:opacity-50">GitHub profile</a></span
+		>. Sometimes even complete projects!
+	</p>
+
+	<p>
+		Have a crazy idea? Let's chat about it, we will love it, <span
+			class="text-primary text-lg font-bold"
+			><a href="/" class="underline hover:opacity-50">get in touch</a></span
+		>.
+	</p>
+
+	<ul class="mb-8 list-inside list-disc space-y-2 pl-6">
+		<li>Check our products and our simple pricing.</li>
+		<li>
+			Check out all of our archived experiments
+			<a href="/" class="underline">here</a>.
+		</li>
+		<li>Check out our open-source libraries & projects.</li>
+	</ul>
+
+	<p>
+		Let's build stuff,<br />
+		<i class="font-bold text-white">~The Futurewise Team</i>
+	</p>
+</section>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,7 @@
 	import PillFlower from '$lib/components/PillFlower.svelte';
 	import TitleFullWidth from '$lib/components/TitleFullWidth.svelte';
 	import Quote from '$lib/components/Home/Quote.svelte';
+	import About from '$lib/components/Home/About.svelte';
 </script>
 
 <!-- Products Slider Section -->
@@ -34,100 +35,4 @@
 <Quote />
 
 <!-- About Section -->
-<section class="hidden px-6 py-12 text-gray-300 md:px-12 lg:px-16">
-	<div class="container mx-auto max-w-4xl">
-		<p class="mb-6">
-			<span class="text-fw-red text-2xl font-bold">W</span>e are an innovation & development lab
-			experimenting in the intersection of e-Commerce, Payments, AI and Machines, with a focus in
-			LATAM
-		</p>
-
-		<p class="mb-6">
-			We have fun experimenting with technology... We build real-world applications & machines.
-		</p>
-
-		<p class="mb-6">
-			We are minimalists & efficient. We aim at building performant systems with little. We use what
-			it's best, we do not reinvent wheels. No charge for it. We are <span
-				class="text-fw-red font-bold">techno-optimists</span
-			>.
-		</p>
-
-		<p class="mb-8">We design, we experiment, we ship!</p>
-
-		<p class="mb-6">
-			<span class="text-fw-red text-2xl font-bold">W</span>e are also a platform for developing
-			Digital Products. We offer our infrastructure, APIs and libraries to experimental projects.
-		</p>
-
-		<p class="mb-6">
-			Some experiments fail, do not reach the level of products, so they remain as acquired
-			knowledge in the form of libraries, designs, and articles.
-		</p>
-
-		<p class="mb-6">
-			Sometimes ideas do not leave their form, so we let them mature in the <span class="underline"
-				>Idea Lab</span
-			>.
-		</p>
-
-		<p class="mb-6">
-			<span class="text-fw-red text-2xl font-bold">F</span>ortunately our base tech stack is very
-			simple yet powerful: <span class="font-bold">Swift</span> &
-			<span class="font-bold">Figma</span>
-			for the front-end, <span class="font-bold">NextJS</span> for our
-			<span class="font-bold">Python</span> for AI, <span class="font-bold">MicroPython</span> for
-			embedded systems, and <span class="font-bold">Docker</span> for containerization,
-			<span class="font-bold">Bitcoin</span> / <span class="font-bold">USDT</span> for value
-			preservation & money transmission,
-			<span class="font-bold">ArWeave</span>, <span class="font-bold">Render</span> &
-			<span class="font-bold">AWS</span> for cloud infrastructure.
-		</p>
-
-		<p class="mb-6">
-			<span class="text-fw-red text-2xl font-bold">W</span>e believe in open-protocols, so we are
-			also experimenting on building for <span class="font-bold">Nostr</span>.
-		</p>
-
-		<p class="mb-6">
-			Sometimes we use our expertise to contribute to other projects. In these Joint Ventures we
-			participate as technological partners, providing our infrastructure, APIs & team.
-		</p>
-
-		<p class="mb-6">
-			<span class="text-fw-red text-2xl font-bold">W</span>e first solve our own problems through
-			technology, and then make valuable products out of it.
-		</p>
-
-		<p class="mb-6">
-			We also curate a list of third party Tools that we ourselves use on a daily basis, aiming to
-			share with others not only our own stuff, but what it's best out there.
-		</p>
-
-		<p class="mb-6">
-			We nurse from open-source, so we contribute to open-source. Many of the libraries we create
-			for our products can be found in our <span class="underline">GitHub profile</span>. Sometimes
-			even complete projects!
-		</p>
-
-		<p class="mb-8">
-			Have a crazy idea? Let's chat about it, we will love it, <span class="font-bold"
-				>get in touch!</span
-			>
-		</p>
-
-		<ul class="mb-8 list-inside list-disc space-y-2 pl-6">
-			<li><span class="text-fw-red">▶</span> Check our products and our simple pricing.</li>
-			<li>
-				<span class="text-fw-red">▶</span> Check out all of our archived experiments
-				<span class="underline">here</span>.
-			</li>
-			<li><span class="text-fw-red">▶</span> Check out our open-source libraries & projects.</li>
-		</ul>
-
-		<p class="mb-4">
-			Let's build stuff,<br />
-			<span class="italic">~The Futurewise Team</span>
-		</p>
-	</div>
-</section>
+<About />
