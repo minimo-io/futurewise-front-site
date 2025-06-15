d980f2defb31742dceb83cc616fc645c2cccc5ab
Nicolas Erramuspe <nicolas@minimo.io>
2025-06-15 17:41:03 -0300

    All sistems functioning link

--- src/lib/components/SystemStatus.svelte
+++ src/lib/components/SystemStatus.svelte
@@ -1,10 +1,16 @@
 <script lang="ts">
+	import { m } from '$paraglide/messages';
 	import { Circle } from '@lucide/svelte';
 </script>
 
-<a href="/#status" class="text-primary mt-2 flex items-center text-sm text-[15px] leading-5">
+<a
+	href="https://stats.uptimerobot.com/pPaLEt4eBM"
+	target="_blank"
+	rel="nofollow noreferrer"
+	class="text-primary mt-2 flex items-center text-sm text-[15px] leading-5"
+>
 	<div>
 		<Circle class="h-3" fill="#ff4016" />
 	</div>
-	<div class="font-sans">Todos os sistemas funcionando</div>
+	<div class="font-sans">{m.allSystemsWorking()}</div>
 </a>
