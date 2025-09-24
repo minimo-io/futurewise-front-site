8768b03336cd3db9ad1f631a48f8f328ac8a7a09
Nicolas Erramuspe <nicolas@minimo.io>
2025-09-24 18:32:36 -0300

    remove kbd shortcut from loggin button

--- src/lib/components/Buttons/LoginDashboardButton.svelte
+++ src/lib/components/Buttons/LoginDashboardButton.svelte
@@ -20,10 +20,10 @@
 		class="bg-base-200 hover:bg-base-100 flex items-center rounded-full px-5 py-3 text-[13px] font-light uppercase md:px-6 md:text-[15px]"
 	>
 		<span class="relative top-[1px] flex items-center">
-			<span class="mr-0 md:mr-[5px]">{m.login()}</span>
-			<kbd class="kbd kbd-sm relative -top-[2px] hidden md:inline-block">
+			<span class="mr-0">{m.login()}</span>
+			<!-- <kbd class="kbd kbd-sm relative -top-[2px] hidden md:inline-block">
 				<span>L</span>
-			</kbd>
+			</kbd> -->
 		</span>
 	</a>
 {/if}
