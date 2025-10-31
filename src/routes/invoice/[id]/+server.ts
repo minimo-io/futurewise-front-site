c30cdf1ed14778b8eb36a4dd1bbb1f7e5b0c1a5b
minimo-io <nicolas@minimo.io>
2025-10-31 14:29:08 -0300

    new invoices

--- src/routes/invoice/[id]/+server.ts
+++ src/routes/invoice/[id]/+server.ts
@@ -1,9 +1,13 @@
 import { error } from '@sveltejs/kit';
 import type { RequestHandler } from './$types';
 import pdf1 from '$lib/data/invoices/123e4567-e89b-12d3-a456-426614174000.pdf?url';
+import pdf2 from '$lib/data/invoices/7f2a4e2a-bb83-4a3f-8e56-bb7a7c3e2a4d.pdf?url';
+import pdf3 from '$lib/data/invoices/c1a8b3b4-2f37-4e1b-9dfb-3c76ad3c87f4.pdf?url';
 
 const invoices: Record<string, string> = {
-	'123e4567-e89b-12d3-a456-426614174000': pdf1
+	'123e4567-e89b-12d3-a456-426614174000': pdf1,
+	'7f2a4e2a-bb83-4a3f-8e56-bb7a7c3e2a4d': pdf2,
+	'c1a8b3b4-2f37-4e1b-9dfb-3c76ad3c87f4': pdf3
 };
 
 export const GET: RequestHandler = async ({ params, fetch }) => {
